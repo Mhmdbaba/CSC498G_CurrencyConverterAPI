cfd2aec27afc3e19f2dfc6900985fa9dd8037f78
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-03-29 14:57:26 +0300

    commit 5

--- app/src/main/java/com/example/currencyconverter/ConversionPage.java
+++ app/src/main/java/com/example/currencyconverter/ConversionPage.java
@@ -3,7 +3,10 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -19,6 +22,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,6 +40,50 @@ public class ConversionPage extends AppCompatActivity {
     private EditText input;
     private TextView text_view;
     ImageView img;
+
+    public class Query extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+
+            try {
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                //Read the output of API
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute (String s){
+            //Log.i("Result: ", s);
+            super.onPostExecute(s);
+
+            try{
+                JSONObject json = new JSONObject(s);
+                String creayed_at = json.getString("created_at");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +98,10 @@ public class ConversionPage extends AppCompatActivity {
     private void hideKeyboard (View view){
         InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getApplicationWindowToken(),0);
+    }
+
+    public void Button(View view){
+        Intent intent = new Intent(this, MainActivity.class);
     }
 
     public void Convert (View view){
@@ -62,6 +119,7 @@ public class ConversionPage extends AppCompatActivity {
         Double output = 0.0;
         if (curr.equalsIgnoreCase("usd")){ //convert to USD
             //output = input....
+
 
             //save to database
 
